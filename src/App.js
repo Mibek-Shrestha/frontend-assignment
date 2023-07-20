19a79a26437e08a186e05c5b461401b4887b9515
mibek-shrestha <mibekstha@gmail.com>
2023-07-20 13:41:41 +0545

    react-router-dom implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import ProductDetails from "./items/ProductDetails";
+const NotFound = () => <h1>404 - Page Not Found</h1>;
 
 const App = () => {
-  return <div>Online Store</div>;
+  return (
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
